58dcb46a7abc5be4b257dc5486261231b2f035da
EvaNavarro20 <evaalondra28@gmail.com>
2025-01-12 02:00:08 +0000

    Solucionario Practica 1 11/01

---
+++ SolucionarioPractica1/Consolidado de Ejercicios.py
@@ -0,0 +1,32 @@
+#Ejercicio4
+print("Hola mundo")
+
+#Ejercicio5
+nombre1=input("ingrese su nombre")
+print("Hola",nombre1)
+
+#Ejercicio6
+Edad1=0
+Edad1=int(input("Ingrese su edad:"))
+if Edad1>=18:
+    print("si es mayor de edad")
+
+else:
+    print("no es mayor de edad")
+
+#Ejercicio7
+
+Num1=0
+Num1=int(input("Ingrese un número entero:"))
+if Num1 %2 == 0:
+    print("si es un número par")
+
+else:
+    print("no es un número par")
+
+#Ejercicio8
+
+Num1=0
+Num1=int(input("Ingrese un número entero:"))
+Formula1 = Num1 * (Num1 + 1) // 2
+print("El resultado es:",Formula1)
